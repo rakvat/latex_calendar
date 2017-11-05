1e71a03076d9a4406c8fa7cd0e29bab805b5bfe6
rakvat <rakvat@users.noreply.github.com>
2017-11-05 11:57:11 -0800

    test setup

---
+++ test_mycalendar.py
@@ -0,0 +1,12 @@
+import unittest
+
+import mycalendar
+
+
+class MyLatexCalendarTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(2+2, 4)
+
+class MyCalendarTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(2+2, 4)
